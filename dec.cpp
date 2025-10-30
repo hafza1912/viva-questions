02a8d4f09d517e2e0a1b7bd51711ddb54647f021
hafza1912 <hafzaazath@gmail.com>
2025-10-30 14:52:28 +0530

    Add files via upload

---
+++ dec.cpp
@@ -0,0 +1,8 @@
+#include<stdio.h>
+int main()
+{
+	int a;
+	a=9;
+	int*b=&+a;
+	printf("%d",*b);
+	}
---
+++ pointer.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+int main()
+{
+	int*ptr=NULL;
+	if(ptr==NULL)
+	printf("NULL");
+	else
+	printf("NOT NULL");
+	return 0;
+	
+}
