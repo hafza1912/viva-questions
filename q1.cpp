d7cede1b743094fbcea08f1a79e62035ca4ac527
hafza1912 <hafzaazath@gmail.com>
2025-10-30 14:39:52 +0530

    Add files via upload

---
+++ q1.cpp
@@ -0,0 +1,16 @@
+//passing arrays to function related questions
+
+#include<stdio.h>
+int main(){
+	#include <stdio.h>
+void printArray(int arr[], int size) {
+   for (int i = 0; i < size; i++) {
+       printf("%d ", arr[i]);
+   }
+}
+int main() {
+   int arr[] = {1, 2, 3, 4, 5};
+   int size = sizeof(arr) / sizeof(arr[0]);
+   printArray(arr, size);
+   return 0;
+}
---
+++ q2.cpp
@@ -0,0 +1,10 @@
+// math library functions
+#include<stdio.h>
+int main(){
+	double a;
+	scanf("%lf",&a);
+	printf("%.0lf",ceil(a));
+	printf("%.0lf",floor(a));
+	printf("%.0lf",round(a)));
+	return 0;
+}
